b6050300438b9f4cd58fde9352a98b0f50e70f44
Hamza2204 <76258180+Hamza2204@users.noreply.github.com>
2021-01-09 18:18:35 -0500

    Add files via upload

---
+++ StopWatch.js
@@ -0,0 +1,62 @@
+let ms = 0, s = 0, m = 0;
+let timer;
+
+const stopwatchEl = document.querySelector('.stopwatch');
+const lapsContainer = document.querySelector('.laps');
+
+function start() {
+    if (!timer) {
+    timer = setInterval(run, 10);
+    }
+}
+
+function run() {
+    stopwatchEl.textContent =  getTimer();
+    ms++;
+    if(ms == 100) {
+        ms = 0;
+        s++;
+    }
+    if (s == 60) {
+        s = 0;
+        m++;
+    }
+}
+
+function pause() {
+    stopTimer();
+}
+
+function stop() {
+    stopTimer();
+    ms = 0;
+    s = 0;
+    m = 0;
+    stopwatchEl.textContent = getTimer();
+}
+
+function stopTimer() {
+    clearInterval(timer);
+    timer = false;
+}
+
+function getTimer() {
+    return (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s) + ':' + (ms < 10 ? '0' + ms : ms);
+}
+
+function restart() {
+    stop();
+    start();
+}
+
+function lap() {
+    if (timer) {
+        const li = document.createElement('li');
+        li.innerText = getTimer();
+        lapsContainer.appendChild(li);
+    }
+}
+
+function resetLaps() {
+    lapsContainer.innerHTML = '';
+}
